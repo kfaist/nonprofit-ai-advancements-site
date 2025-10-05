01cfd2571aec2430da807b2eb78827965f3a1dcf
kfaist <kristabluedoor@gmail.com>
2025-10-05 04:39:17 -0700

    Use env variables for site and base in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+const { ASTRO_SITE, ASTRO_BASE } = process.env;
+
 export default defineConfig({
-  site: 'https://kfaist.github.io/nonprofit-ai-advancements-site',
-  base: '/nonprofit-ai-advancements-site',
+  site: ASTRO_SITE,
+  base: ASTRO_BASE,
   integrations: [sitemap()],
 });
